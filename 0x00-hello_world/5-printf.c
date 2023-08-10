8643edb4f1009614a63aacd6fed138d59d6b39cb
Niiijima <qagwu11@gmail.com>
2023-08-10 08:08:24 -0700

    not give up

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+	print("with proper grammer, but the outcome is a piece of art.\n");
+	return (0);
+}
