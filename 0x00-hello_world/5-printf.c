a16460e72625090a75ceca99f9f5ded16c3048ae
Niiijima <qagwu11@gmail.com>
2023-08-10 12:17:40 -0700

    not give up

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#!/bin/bash
+#inclde<stdio.h>
 /**
  * main - Entry point
  *
- * Return:always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print("with proper grammer, but the outcome is a piece of art.\n");
+	printf("with proper grammer, but the outcome is a piece of art.\n");
 	return (0);
 }
