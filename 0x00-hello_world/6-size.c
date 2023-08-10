3ef4af0075616e306fc9606c9a6f7836fd345778
Niiijima <qagwu11@gmail.com>
2023-08-10 12:05:47 -0700

    not give up

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+
 /**
 * main - Entry ponit
 *
+* print the size of char, int, long int, long long int, float
+*
 * Return: Always 0 (Sucess)
 */
+
 int main(void)
 {
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of short: %zu bytes\n", sizeof(short));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of long double: %zu bytes\n", sizeof(long double));
+	printf("Size of char: %i bytes\n", sizeof(char));
+	printf("Size of short: %i bytes\n", sizeof(short));
+	printf("Size of int: %i bytes\n", sizeof(int));
+	printf("Size of long: %i bytes\n", sizeof(long));
+	printf("Size of long long: %i bytes\n", sizeof(long long));
+	printf("Size of float: %i bytes\n", sizeof(float));
+	printf("Size of double: %i bytes\n", sizeof(double));
+	printf("Size of long double: %i bytes\n", sizeof(long double));
 	return (0);
 }
