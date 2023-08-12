ae140ea2ad57fe91d6c977280d8413c98651b8fe
Niiijima <qagwu11@gmail.com>
2023-08-11 20:48:20 -0700

    wow

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
   */
 int main(void)
 {
-	char alphabets[26]="abcdefghijklmnopqrstuvwxyz";
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	
-	for (i=0; i<26; i++)
+
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
